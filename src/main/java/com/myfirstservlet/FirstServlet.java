164d8477e59867fab5d3199cedba97176b39d138
Kisalay <kisalaykisu@gmail.com>
2022-08-26 21:44:58 +0530

    UC1 has been added:

--- src/main/java/com/myfirstservlet/FirstServlet.java
+++ src/main/java/com/myfirstservlet/FirstServlet.java
@@ -7,14 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-@WebServlet("/santhosh")
+@WebServlet("/kisalay")
 
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h3>Hello World! Santhosh's First Servlet</h3>");
+        out.println("<h3>Hello Kisalay's First Servlet</h3>");
         out.close();
     }
 }
